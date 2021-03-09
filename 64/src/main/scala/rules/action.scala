9e9eb23624645d11b914aa61712a73197a42b84d
erg <hugo.ludemann@gmail.com>
2021-03-09 20:28:28 +1100

    sketched action api

--- 64/src/main/scala/rules/action.scala
+++ 64/src/main/scala/rules/action.scala
@@ -1,10 +1,36 @@
 package rules 
 
-/* On each turn, a player may make an action.
- * Examples:
- *  - Change the color of a square; 
- *  - Place a token on a square; 
- *  - Remove a token from a square;
- *  - Move a token from a square to another;
- */
-object Rules
+import board.{ Region, Token, Line }
+
+object Action {
+  
+  // Not sure yet how to link these to an actual board. At this point 
+  // it looks like they are side effecting funtions, acting on a mutable 
+  // board object.
+
+
+
+
+
+  // Atomic actions.
+  def remove(token: Token, region: Region, callback: Any = ()): Unit = ???
+  def    put(token: Token, region: Region, callback: Any = ()): Unit = ???
+
+
+  // More complex actions built from actomic ones.
+  def move(token: Token, from: Region, to: Region): Unit =
+    remove(token, from, callback = put(token, to))
+  def rotate(token: Token, region: Region) = 
+    remove(token, region, callback = put(token.rotate(1), region))
+  
+  // Monkey swings along a vine.
+  val monkey = Token('M, 0)
+  def moveMonkey(from: Region, to: Region) = 
+    if (!from.data.lines.contains(Line(to, false))) throw new Exception("no vine for monkey")
+    else remove(monkey, from, callback=put(monkey, to))
+
+
+
+
+}
+
