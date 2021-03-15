5ae89a2519d037dfab58f7652be9f2a266d0c2f5
erg <hugo.ludemann@gmail.com>
2021-03-15 19:51:15 +1100

    added a comment to justify Magma.scala

--- board/Magma.scala
+++ board/Magma.scala
@@ -1,6 +1,17 @@
 package board
 
-// Magma type class.
+/** Magma typeclass. 
+ *
+ *  Regions are often joined together during board generation.
+ *  Whatever properties each of the consituent regions has must
+ *  be able to be combined to define the value of the new larger
+ *  region in some way. For example, if you join a blue region
+ *  with a yellow one, you might expect a green one.
+ *
+ *  Could have used a semigroup from cats or somewhere, but the 
+ *  combine operations here are not required to be accosicative
+ *  and we don't need all the cats extras for now.
+ */
 trait Magma[T] {
   def combine(x: T, y: T): T
 }
