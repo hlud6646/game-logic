a7ec21362d5b84270251614e5666572df0f9378b
Hugo Ludemann <hugo.ludemann@gmail.com>
2021-03-23 11:09:30 +1100

    Update Token.scala

--- main/scala/board/Token.scala
+++ main/scala/board/Token.scala
@@ -2,12 +2,6 @@ package board
 
 /** A token placed on a square, as in a chess piece or a disk in checkers.
  *  Animal can only be "E" for elephant, "R" for Rat "M" for Monkey or "A" for Amoeba.
- *  Should we use a refined type here or some other implementation? Using string 
- *  when only four single character values are allowed seems smelly.
- *
- *  Since the animal is actually only allowed to be one of four values, 
- *  String is not the right type. 
- *  TODO: Migrate to the adt defined below.
  */
 
 case class Token(
