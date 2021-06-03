6c272d7ef4298a348af7eea1ae7c38df602123f4
hlud6646 <hugo.ludemann@gmail.com>
2021-06-03 22:30:16 +1000

    tidied up again

--- main/scala/game/Game.scala
+++ main/scala/game/Game.scala
@@ -1,17 +1,13 @@
 package game
 
+import Types._
 import board.{ Board, Player }
 import game.Input
-import Types._
 
-import monocle.macros.Lenses
-
-@Lenses case class GameState(boardState: Board, toMove: Player) {
+case class GameState(boardState: Board, toMove: Player) {
   def push(m: Move) = GameState(m(boardState), toMove.otherPlayer)
 }
 
-// A specific modification to the board state, assumed but not required to be 
-// small, e.g. remove the Monkey token from the (2, 1) square.
 case class GameInvariants(p1: UserId, p2: UserId, initialBoard: Board, action: Input => Move)
 
 case class Game(invariants: GameInvariants, state: GameState) {
